fff52133db3948def3f96fc6c28b9943d14bc1ff
monterrozagera <monterrozagerardo@protonmail.com>
2024-01-09 16:57:09 -0800

    refactor

--- manager.py
+++ manager.py
@@ -1,6 +1,6 @@
 from cryptography.fernet import Fernet
 import pass_db
-import argparse
+
 
 class Manager():
     def __init__(self, user, key, database = ""):
@@ -45,78 +45,3 @@ class Manager():
 
     def update_record(self, index, user, password):
         pass_db.update_database(index, self.user, self.f.encrypt(bytes(user, 'utf-8')).decode(), self.f.encrypt(bytes(password, 'utf-8')).decode())
-
-# run as cmd if not imported
-if __name__ == "__main__":
-    arg_parser = argparse.ArgumentParser(
-        prog='PasswordManager v0.1',
-        description='Manage your password, securely.',
-    )
-    arg_parser.add_argument('-u', '--user', type=str)
-    arg_parser.add_argument('-k', '--key', type=str)
-    arg_parser.add_argument('-cu', '--create_user', action='store_true')
-    args = arg_parser.parse_args()
-
-    if args.create_user:
-        new_user = input("Enter your new username: ")
-        new_key = Fernet.generate_key().decode()
-
-        print(f"[!] NEW KEY CREATED, SAVE IT [!]\n\t{new_key}")
-
-        Pass_Manager = Manager(new_user, new_key)
-        Pass_Manager.create_user()
-        exit()
-
-    USER = args.user
-    KEY =  args.key
-
-    print("PasswordManager v0.1")
-    print("\nWhat would you like to do?")
-    print("\n1.Retreive all\n2.Add Password\n3.Delete Record\n4.Delete All Passwords\n5.Update Record\n6.Exit\n")
-    
-    with open(KEY, "r") as key_file:
-        KEY = key_file.read()
-    
-    Pass_Manager = Manager(USER, KEY)
-
-    while True:
-        try:
-            choice = input(">")
-
-            if choice.isnumeric():
-                match int(choice):
-                    case 1:
-                        Pass_Manager.print_all_passwords()
-                    case 2:
-                        name = input("Name: ")
-                        password = input("Password: ")
-                        Pass_Manager.save_to_database(name, password)
-
-                    case 3:
-                        id = input("ID of record you wish to delete>")
-
-                        if id.isnumeric():
-                            Pass_Manager.delete_from_db(int(id))
-
-                    case 4:
-                        delete = input("Delete all records? y/n>")
-                        if delete == "y":
-                            Pass_Manager.delete_all_passwords()
-
-                    case 5:
-                        index = input("Which record would you like to update? (index)")
-
-                        if index.isnumeric():
-                            name = input("Name: ")
-                            password = input("Password: ")
-                            Pass_Manager.update_record(int(index), name, password)
-
-                    case 6:
-                        pass_db.close_db()
-                        exit()
-
-                    case 99:
-                        print(Pass_Manager.return_all_passwords())
-        except KeyboardInterrupt:
-            pass_db.close_db()
-            exit()
--- pass_db.py
+++ pass_db.py
@@ -1,75 +1,77 @@
 import sqlite3
 
-try:
-    with open("pass.db", 'r') as db:
-        pass
-    conn = sqlite3.connect('pass.db')
-except FileNotFoundError:
-    conn = sqlite3.connect('pass.db')
-
-    conn.execute("""CREATE TABLE PASSWORDS
-                 (id INT PRIMARY KEY    NOT NULL,
-                 user_id TEXT NOT NULL,
-                 pass_id INT NOT NULL,
-                 name   TEXT NOT NULL,
-                 password   TEXT NOT NULL); 
-                 """)
-    
-    conn.execute("""CREATE TABLE USERS
-                 (name  TEXT PRIMARY KEY    NOT NULL,
-                 password   TEXT NOT NULL);
-                 """)
-    
-
-def return_all_records(user) -> list:
-    
-    return conn.execute(f"""SELECT pass_id, name, password FROM passwords WHERE user_id = ?""", (user,))
-
-
-def add_to_database(id: int, user: str, pass_id: int, name: str, password: str):
-    conn.execute(f"""INSERT INTO PASSWORDS (ID,USER_ID,PASS_ID,NAME,PASSWORD)
-                 VALUES (?, ?, ?, ?, ?)""", (id, user, pass_id, name, password))
-    conn.commit()
-
-def create_user(name: str, password: str):
-    conn.execute(f"""INSERT INTO users (name,password)
-                 VALUES (?, ?)""", (name, password))
-    conn.commit()
-
-def update_database(id, user_id, name, password):
-
-    conn.execute(f"""UPDATE PASSWORDS
-                     SET name = ? , password = ?
-                     WHERE pass_id = ? AND user_id = ?""", (name, password, id, user_id))
-    conn.commit()
-
-def delete_from_database(id: int):
-    conn.execute(f"""DELETE FROM PASSWORDS
-                 WHERE ID = ?""", (id,))
-    conn.commit()
-
-def delete_all(user_id):
-    conn.execute("DELETE FROM PASSWORDS WHERE user_id = ?", (user_id,))
-    conn.commit()
-
-def check_current_index() -> int:
-    query = conn.execute(f"""SELECT id from PASSWORDS""")
-
-    try:
-        
-        return query.fetchall()[-1][0]
-    except IndexError:
-        return 0
-
-def check_current_pass_index(user) -> int:
-    try:
-        query = conn.execute(f"""SELECT pass_id FROM passwords WHERE user_id = ?""", (user,))
+class Database():
+    def init(self, path):
         try:
+            with open("pass.db", 'r') as db:
+                pass
+            self.conn = sqlite3.connect('pass.db')
+        except FileNotFoundError:
+            self.conn = sqlite3.connect('pass.db')
+
+            self.conn.execute("""CREATE TABLE PASSWORDS
+                        (id INT PRIMARY KEY    NOT NULL,
+                        user_id TEXT NOT NULL,
+                        pass_id INT NOT NULL,
+                        name   TEXT NOT NULL,
+                        password   TEXT NOT NULL); 
+                        """)
+
+            self.conn.execute("""CREATE TABLE USERS
+                        (name  TEXT PRIMARY KEY    NOT NULL,
+                        password   TEXT NOT NULL);
+                        """)
+
+
+    def return_all_records(self, user) -> list:
+
+        return self.conn.execute(f"""SELECT pass_id, name, password FROM passwords WHERE user_id = ?""", (user,))
+
+
+    def add_to_database(self, id: int, user: str, pass_id: int, name: str, password: str):
+        self.conn.execute(f"""INSERT INTO PASSWORDS (ID,USER_ID,PASS_ID,NAME,PASSWORD)
+                    VALUES (?, ?, ?, ?, ?)""", (id, user, pass_id, name, password))
+        self.conn.commit()
+
+    def create_user(self, name: str, password: str):
+        self.conn.execute(f"""INSERT INTO users (name,password)
+                    VALUES (?, ?)""", (name, password))
+        self.conn.commit()
+
+    def update_database(self, id, user_id, name, password):
+
+        self.conn.execute(f"""UPDATE PASSWORDS
+                        SET name = ? , password = ?
+                        WHERE pass_id = ? AND user_id = ?""", (name, password, id, user_id))
+        self.conn.commit()
+
+    def delete_from_database(self, id: int):
+        self.conn.execute(f"""DELETE FROM PASSWORDS
+                    WHERE ID = ?""", (id,))
+        self.conn.commit()
+
+    def delete_all(self, user_id):
+        self.conn.execute("DELETE FROM PASSWORDS WHERE user_id = ?", (user_id,))
+        self.conn.commit()
+
+    def check_current_index(self, ) -> int:
+        query = self.conn.execute(f"""SELECT id from PASSWORDS""")
+
+        try:
+
             return query.fetchall()[-1][0]
         except IndexError:
             return 0
-    except sqlite3.OperationalError as e:
-        return 0
 
-def close_db():
-    conn.close()
+    def check_current_pass_index(self, user) -> int:
+        try:
+            query = self.conn.execute(f"""SELECT pass_id FROM passwords WHERE user_id = ?""", (user,))
+            try:
+                return query.fetchall()[-1][0]
+            except IndexError:
+                return 0
+        except sqlite3.OperationalError as e:
+            return 0
+
+    def close_db(self, ):
+        self.conn.close()
